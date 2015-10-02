584953c919d0e5b8b41b0bed4442ad2543fe46f8
Barry de Graaff <info@barrydegraaff.tk>
2015-10-02 10:42:55 +0200

    add license block

--- sso/login/index.php
+++ sso/login/index.php
@@ -1,4 +1,25 @@
 <?php
+
+/*
+This file is part of the Zimbra ownCloud Zimlet project.
+Copyright (C) 2015  Barry de Graaff
+
+Bugs and feedback: https://github.com/barrydegraaff/owncloud-zimlet/issues
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see http://www.gnu.org/licenses/.
+*/
+
 if($_GET['logoff']=='true')
 {
    header("HTTP/1.1 401 Unauthorized");
