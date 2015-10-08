5ba2dde75d2a8c55c06c774eba22e6d730da8e93
Michele Olivo <mo@zextras.com>
2015-10-08 15:54:44 +0200

    Created a SOAP Service

--- extension/src/com/zextras/owncloud/OwnCloudExtension.java
+++ extension/src/com/zextras/owncloud/OwnCloudExtension.java
@@ -4,9 +4,19 @@ import org.openzal.zal.extension.ZalExtension;
 import org.openzal.zal.extension.ZalExtensionController;
 import org.openzal.zal.extension.Zimbra;
 import org.openzal.zal.log.ZimbraLog;
+import org.openzal.zal.soap.SoapServiceManager;
 
 public class OwnCloudExtension implements ZalExtension
 {
+
+  private final SoapServiceManager mSoapServiceManager;
+  private OwnCloudSOAPService mOwnCloudSoapService;
+
+  public OwnCloudExtension()
+  {
+    mSoapServiceManager = new SoapServiceManager();
+    mOwnCloudSoapService = new OwnCloudSOAPService();
+  }
 
   @Override
   public String getBuildId()
@@ -23,12 +33,14 @@ public class OwnCloudExtension implements ZalExtension
   @Override
   public void startup(ZalExtensionController zalExtensionController, Zimbra zimbra)
   {
+    mSoapServiceManager.register(mOwnCloudSoapService);
     ZimbraLog.mailbox.info("Loaded extension ownCloud.");
   }
 
   @Override
   public void shutdown()
   {
+    mSoapServiceManager.unregister(mOwnCloudSoapService);
     ZimbraLog.mailbox.info("Removed extension ownCloud.");
   }
 }
