aef826014219c9f61bd214aaff049f758b09f6e4
Michele Olivo <mo@zextras.com>
2016-03-18 09:34:27 +0100

    Removed internal Proxy. Starting from OC9 the  internal proxy cannot handle Dav requests from the web client.

--- extension/src/com/zextras/dav/DavExtension.java
+++ extension/src/com/zextras/dav/DavExtension.java
@@ -2,11 +2,9 @@ package com.zextras.dav;
 
 import com.zextras.dav4zimbra.Dav4ZimbraSOAPService;
 import com.zextras.owncloud.OwnCloudSOAPService;
-import com.zextras.owncloud.proxy.OwnCloudProxyHandler;
 import org.openzal.zal.extension.ZalExtension;
 import org.openzal.zal.extension.ZalExtensionController;
 import org.openzal.zal.extension.Zimbra;
-import org.openzal.zal.http.HttpServiceManager;
 import org.openzal.zal.log.ZimbraLog;
 import org.openzal.zal.soap.SoapServiceManager;
 
@@ -19,22 +17,18 @@ import java.lang.ref.WeakReference;
 public class DavExtension implements ZalExtension
 {
   private final SoapServiceManager mSoapServiceManager;
-  private final HttpServiceManager mHttpServiceManager;
   private final DavSOAPService mDavSoapService;
   private final Dav4ZimbraSOAPService mDav4ZimbraSoapService;
   private final OwnCloudSOAPService mOwnCloudSoapService;
-  private final OwnCloudProxyHandler mOwnCloudProxyHandler;
   private final Zimbra mZimbra;
 
   public DavExtension()
   {
     mZimbra = new Zimbra();
     mSoapServiceManager = new SoapServiceManager();
-    mHttpServiceManager = new HttpServiceManager();
     mDavSoapService = new DavSOAPService(mZimbra.getProvisioning());
     mDav4ZimbraSoapService = new Dav4ZimbraSOAPService(mZimbra.getMailboxManager(), mZimbra.getProvisioning());
     mOwnCloudSoapService = new OwnCloudSOAPService(mZimbra.getProvisioning());
-    mOwnCloudProxyHandler = new OwnCloudProxyHandler(mZimbra.getProvisioning());
   }
 
   @Override
@@ -60,7 +54,6 @@ public class DavExtension implements ZalExtension
     mSoapServiceManager.register(mDavSoapService);
     mSoapServiceManager.register(mDav4ZimbraSoapService);
     mSoapServiceManager.register(mOwnCloudSoapService);
-    mHttpServiceManager.registerHandler(mOwnCloudProxyHandler);
     ZimbraLog.mailbox.info("Loaded WebDav extension.");
   }
 
@@ -73,7 +66,6 @@ public class DavExtension implements ZalExtension
     mSoapServiceManager.unregister(mDavSoapService);
     mSoapServiceManager.unregister(mDav4ZimbraSoapService);
     mSoapServiceManager.unregister(mOwnCloudSoapService);
-    mHttpServiceManager.unregisterHandler(mOwnCloudProxyHandler);
     ZimbraLog.mailbox.info("Removed extension ownCloud.");
   }
 }
