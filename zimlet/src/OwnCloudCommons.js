f4f1a35daddff16337364cf244919f6292569780
barrydegraaff <info@barrydegraaff.tk>
2016-07-25 10:02:42 +0200

    add quotes to handle spaces in filenames on attach from owncloud (via attach dialog)

--- zimlet/src/OwnCloudCommons.js
+++ zimlet/src/OwnCloudCommons.js
@@ -149,7 +149,7 @@ OwnCloudCommons.prototype._getResourceCbk = function(resource, resources, ids, c
   req.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
   req.setRequestHeader('Content-Type',  'application/octet-stream' + ';');
   req.setRequestHeader('X-Zimbra-Csrf-Token', window.csrfToken);
-  req.setRequestHeader('Content-Disposition', 'attachment; filename='+ resource.getName() + ';');
+  req.setRequestHeader('Content-Disposition', 'attachment; filename="'+ resource.getName() + '";');
   req.onload = (function(_this, resources, ids, callback) {
     return function(result) {
       var resp = eval('[' + this.responseText + ']'),
