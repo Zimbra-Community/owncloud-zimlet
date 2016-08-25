2d8e8b0f22cf8098c2d0a29d585775aa1839021d
Barry de Graaff <info@barrydegraaff.tk>
2016-08-25 16:18:18 +0200

    add dummy password

--- extension/src/com/zextras/dav/UploadHandler.java
+++ extension/src/com/zextras/dav/UploadHandler.java
@@ -122,7 +122,7 @@ public class UploadHandler implements HttpHandler
         Integer.parseInt(userProperties.get(ZimletProperty.DAV_SERVER_PORT)),
         userProperties.get(ZimletProperty.DAV_SERVER_PATH),
         userProperties.get(ZimletProperty.DAV_USER_USERNAME),
-        "to-do zimbraContext.getParameter(\"dav_user_password\", "")"
+        "to-do zimbraContext.getParameter(dav_user_password, )"
       );
 
       ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
