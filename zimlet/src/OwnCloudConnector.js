8a405e34d02ea3e7fdf701c15b533e37a093d980
barrydegraaff <info@barrydegraaff.tk>
2016-08-26 08:33:49 +0200

    password via soap to ownCloud connector

--- zimlet/src/OwnCloudConnector.js
+++ zimlet/src/OwnCloudConnector.js
@@ -183,6 +183,7 @@
       );
     }
     soapDoc.set('action', action);
+    soapDoc.set('dav_user_password', tk_barrydegraaff_owncloud_zimlet_HandlerObject.settings['owncloud_zimlet_password']);
     var params = {
       soapDoc: soapDoc,
       asyncMode: true,
