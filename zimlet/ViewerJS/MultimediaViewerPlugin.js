d1f289d45257d6cbaa5f48edf1b3aae6dd858842
barrydegraaff <info@barrydegraaff.tk>
2016-10-14 07:06:45 +0200

    set preferred method to html5

--- zimlet/ViewerJS/MultimediaViewerPlugin.js
+++ zimlet/ViewerJS/MultimediaViewerPlugin.js
@@ -31,7 +31,7 @@ function MultimediaViewerPlugin() {
 		viewerElement.style.overflow = "auto";
 		
 		// init viewerjs
-		videojs(document.getElementById('multimedia_viewer'), {controls:'enabled', techOrder:['flash','html5']}, function() {
+		videojs(document.getElementById('multimedia_viewer'), {controls:'enabled', techOrder:['html5','flash']}, function() {
 		  // This is functionally the same as the previous example.
 		});
 
