e78e2312ca2e50a3d520637f7dd777a57279177d
barrydegraaff <info@barrydegraaff.tk>
2017-05-03 19:47:06 +0200

    fix permissions script

--- kvm/setpermissions.sh
+++ kvm/setpermissions.sh
@@ -2,10 +2,10 @@
 
 ocpath='/var/www/html/nextcloud/'
 
-if ! [ -d \$ocpath ]
+if ! [ -d $ocpath ]
 then
-	echo "/var/www/html/ not found, abort!"
-	exit 0;
+echo "/var/www/html/ not found, abort!"
+exit 0;
 fi
 
 htuser='apache'
@@ -14,35 +14,35 @@ htgroup='apache'
 rootuser='root'
 
 printf "Creating possible missing Directories\n"
-mkdir -p \$ocpath/data
-mkdir -p \$ocpath/assets
-mkdir -p \$ocpath/updater
+mkdir -p $ocpath/data
+mkdir -p $ocpath/assets
+mkdir -p $ocpath/updater
 
 printf "chmod Files and Directories\n"
-find \${ocpath}/ -type f -print0 | xargs -0 chmod 0640
-find \${ocpath}/ -type d -print0 | xargs -0 chmod 0750
+find ${ocpath}/ -type f -print0 | xargs -0 chmod 0640
+find ${ocpath}/ -type d -print0 | xargs -0 chmod 0750
 
 printf "chown Directories\n"
-chown -R \${rootuser}:\${htgroup} \${ocpath}/
-chown -R \${htuser}:\${htgroup} \${ocpath}/apps/
-chown -R \${htuser}:\${htgroup} \${ocpath}/assets/
-chown -R \${htuser}:\${htgroup} \${ocpath}/config/
-chown -R \${htuser}:\${htgroup} \${ocpath}/data/
-chown -R \${htuser}:\${htgroup} \${ocpath}/themes/
-chown -R \${htuser}:\${htgroup} \${ocpath}/updater/
+chown -R ${rootuser}:${htgroup} ${ocpath}/
+chown -R ${htuser}:${htgroup} ${ocpath}/apps/
+chown -R ${htuser}:${htgroup} ${ocpath}/assets/
+chown -R ${htuser}:${htgroup} ${ocpath}/config/
+chown -R ${htuser}:${htgroup} ${ocpath}/data/
+chown -R ${htuser}:${htgroup} ${ocpath}/themes/
+chown -R ${htuser}:${htgroup} ${ocpath}/updater/
 
-chmod +x \${ocpath}/occ
+chmod +x ${ocpath}/occ
 
 printf "chmod/chown .htaccess\n"
-if [ -f \${ocpath}/.htaccess ]
+if [ -f ${ocpath}/.htaccess ]
  then
-  chmod 0644 \${ocpath}/.htaccess
-  chown \${rootuser}:\${htgroup} \${ocpath}/.htaccess
+  chmod 0644 ${ocpath}/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/.htaccess
 fi
-if [ -f \${ocpath}/data/.htaccess ]
+if [ -f ${ocpath}/data/.htaccess ]
  then
-  chmod 0644 \${ocpath}/data/.htaccess
-  chown \${rootuser}:\${htgroup} \${ocpath}/data/.htaccess
+  chmod 0644 ${ocpath}/data/.htaccess
+  chown ${rootuser}:${htgroup} ${ocpath}/data/.htaccess
 fi
 
 semanage fcontext -a -t httpd_sys_rw_content_t '/var/www/html/nextcloud/data(/.*)?'
@@ -63,4 +63,3 @@ setsebool -P httpd_can_network_connect on
 setsebool -P httpd_use_cifs on
 
 setenforce 1
-EOF
