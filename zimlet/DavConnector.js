e251bcc2c3b40e0ffdc2731fde705967adabe9f6
Barry de Graaff <info@barrydegraaff.tk>
2017-11-01 09:17:29 +0100

    fix regression in Internet Explorer 11

--- zimlet/DavConnector.js
+++ zimlet/DavConnector.js
@@ -105,6 +105,20 @@
    */
   DavResource.fromRawResource = function(rawEntity) {
 
+   //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+   //support for endsWith in IE11
+   if (!String.prototype.endsWith)
+     String.prototype.endsWith = function(searchStr, Position) {
+         // This works much better than >= because
+         // it compensates for NaN:
+         if (!(Position < this.length))
+           Position = this.length;
+         else
+           Position |= 0; // round position
+         return this.substr(Position - searchStr.length,
+                            searchStr.length) === searchStr;
+     };
+
      if(rawEntity.href.endsWith("/"))
      {
        rawEntity.contentType = 'httpd/unix-directory';
