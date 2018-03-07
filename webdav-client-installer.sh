c0c368ae8112ef6c1880a41a95b1f0dd4ded80a3
Adrian Gibanel bTactic <adrian.gibanel@btactic.com>
2018-03-07 17:46:25 +0100

    FIXED: config_template.xml was generated before fetching prop2xml.jar

--- webdav-client-installer.sh
+++ webdav-client-installer.sh
@@ -263,7 +263,6 @@ else
    else
       chown zimbra:zimbra $TMPFOLDER -R
       su - zimbra -c "zmzimletctl deploy $TMPFOLDER/owncloud-zimlet/zimlet/tk_barrydegraaff_owncloud_zimlet.zip"
-      su - zimbra -c "java -jar $TMPFOLDER/upgrade/prop2xml.jar tk_barrydegraaff_owncloud_zimlet ${OWNCLOUD_EXTENSION_PATH}/config.properties ${OWNCLOUD_ZIMLET_PATH}/config_template.xml"
       su - zimbra -c "zmzimletctl configure ${OWNCLOUD_ZIMLET_PATH}/config_template.xml"
       su - zimbra -c "zmprov fc all"
    fi
