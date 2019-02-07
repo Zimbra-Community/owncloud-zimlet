36be1141690105193665ffdcd30c3756e771141f
Barry de Graaff <info@barrydegraaff.tk>
2019-02-07 19:42:01 +0100

    Update install-opcache.sh

--- kvm/install-opcache.sh
+++ kvm/install-opcache.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y php70u-opcache
+yum install -y php72u-opcache
 
 wget https://raw.githubusercontent.com/Zimbra-Community/owncloud-zimlet/soapServiceBarry/kvm/10-opcache.ini -O /etc/php.d/10-opcache.ini
 
